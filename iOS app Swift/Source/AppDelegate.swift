a6c3e5ad8b0d6d27ef0369121dbbc87294f1d63d
Douglas Hill <hello@douglashill.co>
2016-09-01 22:05:55 +0100

    Fix incorrect type
    Thanks a lot Swift 3 migrator!

--- iOS app Swift/Source/AppDelegate.swift
+++ iOS app Swift/Source/AppDelegate.swift
@@ -6,7 +6,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
         self.window = window
         window.rootViewController = ViewController()
